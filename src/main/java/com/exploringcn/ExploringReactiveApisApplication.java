da16e75adc1fb0797b3b9e39e62606a865437f17
exploringcn21 <exploringcn21@gmail.com>
2021-08-25 19:48:40 +0100

    Initial commit

---
+++ src/main/java/com/exploringcn/ExploringReactiveApisApplication.java
@@ -0,0 +1,13 @@
+package com.exploringcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringReactiveApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringReactiveApisApplication.class, args);
+	}
+
+}
