83b98a0f3df7f8dafdaeba16d7491b76b804c1a1
exploringcn21 <exploringcn21@gmail.com>
2021-08-31 23:18:12 +0100

    added API endpoint to return item by ID

--- src/main/java/com/exploringcn/controller/ItemController.java
+++ src/main/java/com/exploringcn/controller/ItemController.java
@@ -5,10 +5,14 @@ import com.exploringcn.document.Item;
 import com.exploringcn.repository.ItemReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static com.exploringcn.constants.ItemConstants.ITEM_ENDPOINT_V1;
 
@@ -24,4 +28,10 @@ public class ItemController {
         return itemReactiveRepository.findAll();
     }
 
+    @GetMapping(ITEM_ENDPOINT_V1+"/{itemId}")
+    public Mono<ResponseEntity<Item>> getItem(@PathVariable String itemId){
+        return itemReactiveRepository.findById(itemId)
+                .map((item) -> new ResponseEntity<>(item, HttpStatus.OK))   // return response wrapped in Mono along with 200 status
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));    // return NOT FOUND response wrapped in Mono
+    }
 }
